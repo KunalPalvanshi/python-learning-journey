55beaf27f8c0c5b4cf1f129708580dd8c3d1fd0a
Kunal Pal <palvanshikunal@gmail.com>
2026-07-21 09:16:36 +0530

    Create chapter01.py

---
+++ chapter01.py
@@ -0,0 +1,64 @@
+print(''' Twinkle, twinkle, little star,
+How I wonder what you are!
+Up above the world so high,
+Like a diamond in the sky.
+
+When the blazing sun is gone,
+When he nothing shines upon,
+Then you show your little light,
+Twinkle, twinkle, all the night.
+
+Then the traveler in the dark
+Thanks you for your tiny spark,
+How could he see where to go,
+If you did not twinkle so?
+
+In the dark blue sky you keep,
+Often through my curtains peep
+For you never shut your eye,
+Till the sun is in the sky.
+
+As your bright and tiny spark
+Lights the traveler in the dark,
+Though I know not what you are,
+Twinkle, twinkle, little star.''')
+
+
+
+
+#This is my name 
+
+print("Name: Kunal")
+
+#This is my Age 
+print("Age:22")
+
+
+# This is my cit
+print("City: Meerut") 
+
+
+
+
+#This is the Practise of Module import 
+import math
+
+print(math.pi)
+print(math.floor(7.8))
+
+
+#Example of Random Module 
+import random
+print(random.choice)
+
+
+#datetime print 
+import datetime
+today_date  = datetime.date.today()
+print(today_date)
+
+# factorial
+
+import math
+factorial = math.factorial(5)
+print(factorial)
